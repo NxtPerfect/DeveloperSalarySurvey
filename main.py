28ecceeefa7f15dbdac5296dbfdaac477157d45e
NxtPerfect <alakaxan@protonmail.com>
2025-02-24 17:31:28 +0100

    feat: added expertise salary bar plot
    feat: added residence salary bar plot

--- main.py
+++ main.py
@@ -18,18 +18,62 @@ import streamlit as st
 
 def run():
     df = pd.read_csv("salaries.csv")
-    print(df)
-    df = df.rename(columns={
+    renameColumnsToEnglish(df)
+    renameRowsToEnglish(df)
+    # Norwegian currency to eur
+    # 1 NOK = 0.085706417 EUR
+    df.salary = (df.salary * 0.085706417).round(2)
+
+    st.dataframe(df)
+    # View expertise/salary plot
+    expertiseOnSalary = sb.barplot(x="expertise", y="salary", data=df)
+    plt.xticks(rotation=45)
+    ylabels = ['{:,.0f}'.format(y) for y in expertiseOnSalary.get_yticks()]
+    expertiseOnSalary.set_yticklabels(ylabels)
+
+    st.pyplot(expertiseOnSalary.get_figure())
+    # For some reason plot doesn't clear itself
+    # If i don't show it, it gets merged to the next one
+    st.pyplot(plt.show())
+
+    # residence/salary plot
+    residenceOnSalary = sb.barplot(x="residence", y="salary", data=df)
+    ylabels = ['{:,.2f}'.format(y) for y in residenceOnSalary.get_yticks()]
+    residenceOnSalary.set_yticklabels(ylabels)
+    plt.xticks(rotation=45)
+    st.pyplot(residenceOnSalary.get_figure())
+
+def renameColumnsToEnglish(df: pd.DataFrame) -> None:
+    df.rename(columns={
         "kjønn" : "gender",
         "utdanning" : "education",
-        "erfaring" : "years of experience",
-        "arbeidssted" : "region of residence",
+        "erfaring" : "experience",
+        "arbeidssted" : "residence",
         "arbeidssituasjon" : "employment type and sector",
         "fag" : "expertise",
-        "lønn" : "salary"})
-    print(df)
-    ax = sb.lineplot(x="expertise", y="salary", data=df)
-    st.pyplot(plt.show())
+        "lønn" : "salary"}, inplace=True)
+
+def renameRowsToEnglish(df: pd.DataFrame) -> None:
+    df.replace({'bonus?': {"Ja": "Yes", "Nei": "No"}}, inplace=True)
+    df.replace({'gender': {"mann": "male", "kvinne": "female", "annet / ønsker ikke oppgi": "other / do not wish to specify"}}, inplace=True)
+    df.replace({'expertise': {
+        "AI / maskinlæring": "AI / machine learning",
+        "annet": "other",
+        "arkitektur": "architecture",
+        "automatisering": "automation",
+        "databaser": "databases",
+        "devops / drift": "devops / operation",
+        "embedded / IOT / maskinvare": "embedded / IOT / hardware",
+        "ledelse/administrativt": "management / administrative",
+        "programvare": "software",
+        "sikkerhet": "safety",
+    }}, inplace=True)
+    df.replace({'employment type and sector': {
+        "frilans / selvstendig næringsdrivende": "freelance / self-employed",
+        "in-house, offentlig/kommunal sektor": "in-house, public/municipal sector",
+        "in-house, privat sektor": "in-house, private sector",
+        "konsulent": "consultant",
+    }}, inplace=True)
 
 if __name__ == "__main__":
     run();
